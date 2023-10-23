7dab3c49e8d8f9634ca384c239f229daf9cfcea1
kimlong <kimlong5244@gmail.com>
2023-10-23 19:55:19 +0700

    blog bl

--- src/components/BlogBox.js
+++ src/components/BlogBox.js
@@ -21,13 +21,13 @@ const BlogBox = ({
       variants={fadeIn(direction, delay)}
       initial="hidden"
       whileInView={"show"}
-      viewport={{ once: false, amount: amount }}
+      viewport={{ once: true, amount: amount }}
       className="flex-1 group overflow-hidden relative border-2 border-white/50 rounded-xl cursor-pointer inline-block"
       style={style}
     >
       <Link to={`/blog/${id}`}>
         {/* overlay */}
-        <div className="bg-black/50 group-hover:bg-transparent w-full h-full z-40 absolute transition-all duration-300"></div>
+        <div className="bg-transparent w-full h-full z-40 absolute transition-all duration-300 p-r"></div>
         {/* img */}
 
         <img
@@ -37,12 +37,12 @@ const BlogBox = ({
         />
 
         {/* pretitle */}
-        <div className="absolute group-hover:-bottom-full left-12 bottom-24 transition-all duration-500 z-50">
-          <span className="text-gradient">Author: {author}</span>
+        <div className="absolute -bottom-full left-12 group-hover:bottom-12 transition-all duration-500 z-50 w-full">
+          <span className="bg-black/50 leading-10 line-clamp-1 w-full px-2 ">Author: {author}</span>
         </div>
         {/* title */}
-        <div className="absolute group-hover:-bottom-full left-12 bottom-12 transition-all duration-500 z-50">
-          <span className="text-3xl text-white line-clamp-1">{title}</span>
+        <div className="absolute group-hover:-bottom-full left-12 bottom-12 transition-all duration-500 z-50 w-full">
+          <span className="text-xl text-white line-clamp-1 leading-10 bg-black/50 px-2 ">{title}</span>
         </div>
       </Link>
     </motion.div>

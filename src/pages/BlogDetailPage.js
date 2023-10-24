dfe1e90e6d37f70c5cfe18036a1d69b4ac9efb1d
kimlong <kimlong5244@gmail.com>
2023-10-25 00:21:23 +0700

    test

--- src/pages/BlogDetailPage.js
+++ src/pages/BlogDetailPage.js
@@ -107,10 +107,7 @@ const BlogDetailPage = ({
                           : "https://images.unsplash.com/photo-1697519755368-84be46c440e9?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                       }
                     /> */}
-                    <meta
-                      property="og:image"
-                      content="https://kimlongchann.online/img.avif"
-                    />
+                
                   </Helmet>
                   {/* date */}
                   <div className="text-center text-blue-400 mb-4 text-xl">

f06667acf9e3d0f7888bfce035e051b55adc38f5
kimlong <kimlong5244@gmail.com>
2023-10-24 23:59:08 +0700

    test

--- src/pages/BlogDetailPage.js
+++ src/pages/BlogDetailPage.js
@@ -109,7 +109,7 @@ const BlogDetailPage = ({
                     /> */}
                     <meta
                       property="og:image"
-                      content="https://kimlongchann.online/img.avif"
+                      content="https://images.unsplash.com/photo-1596526131090-bcbe09e432d3?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     />
                   </Helmet>
                   {/* date */}

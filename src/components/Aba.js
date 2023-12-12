d8e02acb56112869374c25dd7a9d4f3ccb391020
kimlong <kimlong5244@gmail.com>
2023-12-12 11:05:42 +0700

    backdrop

--- src/components/Aba.js
+++ src/components/Aba.js
@@ -3,13 +3,11 @@ import AbaRiel from "../assets/abaRiel.jpg";
 const Aba = ({ setShowQrCode }) => {
   return (
     <div className="relative bg-red-700">
-      <div className="fixed inset-0 backdrop-blur-sm z-[1000]">
+      <div className="fixed inset-0  bg-black/60  z-[1000]">
         <div className=" mt-14 rounded-md text-black overflow-hidden flex flex-col items-center md:w-[400px] md:h-[600px] mx-4 md:mx-auto">
           <img src={AbaRiel} alt="" className="w-full h-full" />
         </div>
-        <span
-          className="w-8 h-8 cursor-pointer bg-red-600 text-white grid place-items-center rounded-full top-4 right-4 md:top-8 md:right-8 absolute"
-        >
+        <span className="w-8 h-8 cursor-pointer bg-red-600 text-white grid place-items-center rounded-full top-4 right-4 md:top-8 md:right-8 absolute">
           ✖
         </span>
       </div>
@@ -18,3 +16,5 @@ const Aba = ({ setShowQrCode }) => {
 };
 
 export default Aba;
+
+// backdrop-brightness-50
